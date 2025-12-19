490077026c130e59f06ad97aef97d3ee1a424553
Tom Greenwood <tomgreenwood1@gmail.com>
2025-12-19 10:38:03 +0000

    vulkan initialization code and fix more linking

--- lib/include/vk_engine.h
+++ lib/include/vk_engine.h
@@ -8,11 +8,24 @@ public:
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
 	struct SDL_Window* _window{ nullptr };
 
-	VulkanEngine &Get();
+	static VulkanEngine &Get();
 
 	//initializes everything in the engine
 	void init();
@@ -25,4 +38,13 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+
+	void init_vulkan();
+	void init_swapchain();
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+	void init_commands();
+	void init_sync_structures();
 };
